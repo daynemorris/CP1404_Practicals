6f4858ad610141a192da55ecd0f6f2e2f4df683a
Dayne Morris <daynemorris14@gmail.com>
2022-10-02 10:44:47 +1000

    Add three files to repo

---
+++ prac 3/exceptions_to_complete.py
@@ -0,0 +1,16 @@
+"""
+CP1404 - Exceptions to complete
+Dayne Morris
+"""
+
+finished = False
+result = 0
+while not finished:
+    try:
+        result = int(input("Enter an integer: "))
+        finished = True
+    except ValueError:
+        print("Please enter a valid integer.")
+print("Valid result is:", result)
+
+
