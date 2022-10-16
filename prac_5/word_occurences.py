2684274e97f7cdb58ed94d5936069e9f61b5642a
Dayne Morris <daynemorris14@gmail.com>
2022-10-16 17:28:00 +1000

    Add word_occurences to repo

--- prac_5/word_occurences.py
+++ prac_5/word_occurences.py
@@ -2,13 +2,19 @@
 """
 Word Occurrences
 Estimate: 20 minutes
-Actual:    minutes
+Actual: 25 minutes
 """
 words_to_number = {}
-words = input("Text: ").split()
+text = input("Text: ")
+words = text.split()
 for word in words:
     amount_of_words = words_to_number.get(word, 0)
     words_to_number[word] = amount_of_words + 1
-for word, number in words_to_number.items():
-    print(f"{word} : {number}")
+
+words = list(words_to_number.keys())
+words.sort()
+
+maximum_words = max(len(word) for word in words)
+for word, number in sorted(words_to_number.items()):
+    print(f"{word:{maximum_words}} : {number}")
 
