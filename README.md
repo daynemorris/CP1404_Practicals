c73ff930b44c92b7447ad197207727a5593b22b5
User <dayne.morris@my.jcu.edu.au>
2022-11-08 17:49:37 +1000

    Add myguitars.py to repo

---
+++ README.md
@@ -0,0 +1,17 @@
+# CP1404(Programming II)
+## Summary
+
+Here lies the practicals committed and pushed by Sir Dayne Morris the First. Is the code clean? Maybe Does the code follow best practice? I hope so... 
+
+If you're reading this while reviewing my code good luck!
+
+### Things I've learnt
+
+- Naming plays an important role in clean code.
+- Spacing improves code readability (for me).
+- Making the code readable as apposed to needing comments is key.
+
+### Programming Patterns
+https://github.com/CP1404/Starter/wiki
+### Practicals
+https://github.com/CP1404/Practicals
--- prac_7/myguitars.py
+++ prac_7/myguitars.py
@@ -1,35 +1,46 @@
 from prac_6.guitar import Guitar
 
-from programming_language import ProgrammingLanguage
-
 
 def main():
     """Read file of programming language details, save as objects, display."""
-    guitars = []
-    # Open the file for reading
-    in_file = open('guitars.csv', 'r')
-    # File format is like: Language,Typing,Reflection,Year
-    # 'Consume' the first line (header) - we don't need its contents
-    in_file.readline()
-    # All other lines are language data
-    for line in in_file:
-        # print(repr(line))  # debugging
-        # Strip newline from end and split it into parts (CSV)
-        parts = line.strip().split(',')
-        # print(parts)  # debugging
-        # Reflection is stored as a string (Yes/No) and we want a Boolean
-        # Construct a ProgrammingLanguage object using the elements
-        # year should be an int
-        guitar = Guitar(parts[0], parts[1], parts[2])
-        # Add the language we've just constructed to the list
-        guitars.append(guitar)
-        guitars.sort()
-    # Close the file as soon as we've finished reading it
-    in_file.close()
-
-    # Loop through and display all languages (using their str method)
+    print("\nList of Guitars:\n")
+    guitars = load_guitars()
     for guitar in guitars:
         print(guitar)
+
+    print("\nAdd a Guitar!\n")
+
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        add_guitar = Guitar(name, year, cost)
+        print(f"{add_guitar}\n")
+        guitars.append(add_guitar)
+        name = input("Name: ")
+    print("\n...snip...\n")
+    print("These are my guitars:")
+
+    for guitar in guitars:
+        print(guitar)
+    save_guitars(guitars)
+
+
+def load_guitars():
+    guitars = []
+    in_file = open('guitars.csv', 'r')
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], parts[1], parts[2])
+        guitars.append(guitar)
+        guitars.sort()
+    return guitars
+
+
+def save_guitars(guitars):
+    out_file = open('guitars1.csv', 'w')
+    for guitar in guitars:
+        out_file.write
 
 
 main()
