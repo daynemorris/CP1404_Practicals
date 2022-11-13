5c708d3dcaf095cbb10db77778a7a825110e7935
User <dayne.morris@my.jcu.edu.au>
2022-11-14 09:26:33 +1000

    Add box layout files to repo

---
+++ prac_8/box_layout_demo.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.lang import Builder
+import random
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        print("test")
+        self.root.ids.output_label.text = "Hello "
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def press_clear(self):
+        """
+        Clear any buttons that have been selected (visually) and reset status text
+        :return: None
+        """
+        self.root.ids.output_label.text = " "
+        self.root.ids.input_name.text = " "
+
+
+BoxLayoutDemo().run()
