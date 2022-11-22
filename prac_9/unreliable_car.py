e8d5d4953fc8f003fff131fa942bb59902591c27
User <dayne.morris@my.jcu.edu.au>
2022-11-22 21:29:11 +1000

    Add final unreliable car file to repo

--- prac_9/unreliable_car.py
+++ prac_9/unreliable_car.py
@@ -5,14 +5,16 @@ import random
 class UnreliableCar(Car):
 
     def __init__(self, name, fuel, reliability):
-        """Initialise a Taxi instance, based on parent class Car."""
+        """Initialise an Unreliable Car instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
+        """Drive an Unreliable Car a certain distance, based on reliability."""
         random_number = float(random.randint(1, 100))
         if random_number >= self.reliability:
             distance = 0
         distance_driven = super().drive(distance)
         return distance_driven
+
 
